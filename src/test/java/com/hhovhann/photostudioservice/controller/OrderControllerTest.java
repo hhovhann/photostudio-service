011b024bebe39eca6e4145534badae35f4d694fd
Hayk Hovhannisyan <haik.hovhanisyan@gmail.com>
2021-07-14 00:49:27 +0400

    Code cleanup and refactoring.

--- src/test/java/com/hhovhann/photostudioservice/controller/OrderControllerTest.java
+++ src/test/java/com/hhovhann/photostudioservice/controller/OrderControllerTest.java
@@ -3,8 +3,6 @@ package com.hhovhann.photostudioservice.controller;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class OrderControllerTest {
 
     @Test
--- src/test/java/com/hhovhann/photostudioservice/service/OrderServiceTest.java
+++ src/test/java/com/hhovhann/photostudioservice/service/OrderServiceTest.java
@@ -3,8 +3,6 @@ package com.hhovhann.photostudioservice.service;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class OrderServiceTest {
 
     @Test
